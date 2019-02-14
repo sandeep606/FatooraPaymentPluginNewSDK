99acb5f78dd1658c0700281b19fa3bef1a8bc1c1
ashishnakoti <ashish.nakoti@evontech.com>
2019-02-14 19:04:07 +0530

    added new methods

--- src/ios/FatooraPaymentPluginNewSDK.m
+++ src/ios/FatooraPaymentPluginNewSDK.m
@@ -1,28 +1,105 @@
 /********* FatooraPaymentPluginNewSDK.m Cordova Plugin Implementation *******/
 
 #import <Cordova/CDV.h>
+#import <MFSDK/MFSDK.h>
 
 @interface FatooraPaymentPluginNewSDK : CDVPlugin {
   // Member variables go here.
+  MFCustomer *customer;
+
 }
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+- (void)presentPaymentModule:(CDVInvokedUrlCommand*)command;
+- (void)testMethod:(CDVInvokedUrlCommand*)command;
+- (void)payNow:(CDVInvokedUrlCommand*)command;
 @end
 
-@implementation FatooraPaymentPluginNewSDK
+@implementation MyFatoorahPaymentPlug
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command
+
+- (void)presentPaymentModule:(CDVInvokedUrlCommand*)command
 {
+
+    [MFSettings.sharedInstance setMerchantWithMerchantCodeWithMerchantCode:@"999999"
+                                                              merchantName:@"Web Pay"
+                                                          merchantUserName:@"testapi@myfatoorah.com"
+                                                          merchantPassword:@"E55D0"
+                                                       merchantReferenceID:@"201454542102"
+                                                         merchantReturnURL:@"https://www.google.co.in"
+                                                          merchantErrorUrl:@"https://www.yahoo.com"
+                                                                      udf1:@""
+                                                                      udf2:@""
+                                                                      udf3:@""
+                                                                      udf4:@""
+                                                                      udf5:@""
+                                                                isTestMode:YES];
+
+                                                                
     CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Added merchant account"];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
+
+
+- (void)testMethod:(CDVInvokedUrlCommand*)command
+{
+                                                                
+    CDVPluginResult* pluginResult = nil;
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"testMethod  account"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)payNow:(CDVInvokedUrlCommand*)command{
+
+    [MFOrderStatusRequest sharedInstance].delegateOrderStatus = self;
+    [self populateCustomerPayment];
+
+    NSMutableArray *productlistLocal = [[NSMutableArray alloc]init];
+    MFProductDetails *productDetail = [[MFProductDetails alloc]init];
+
+    productDetail.productName = @"ABC";
+    productDetail.productPrice = 0.54;
+    productDetail.productQuntity = 1;
+    
+    [productlistLocal addObject:productDetail];
+    [[MFPaymentRequest sharedInstance] payfor_BOTHWithCustomer:customer productList:productlistLocal subTotal:productDetail.productPrice paymentCurrrency:@"KWD"];
+
+    CDVPluginResult* pluginResult = nil;
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"payNow method"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)populateCustomerPayment{
+    
+    customer = [[MFCustomer alloc]init];
+    customer.customerName = @"customer name "; //Required
+    customer.customerEmailAddress = @"email address";//Required
+    customer.customerMobileNo = @"mobile no";//Required
+    customer.customerGender = @"";
+    customer.customerDOB = @"";
+    customer.customerCivilID = @"";
+    customer.customerArea = @"";
+    customer.customerBlockNo = @"";
+    customer.customerStreet = @"";
+    customer.customerAvenue = @"";
+    customer.customerBuildingNo = @"";
+    customer.customerFloorNo = @"";
+    customer.customerApartment = @"";
+    
+}
+
+
+//Delegate method to be called when payment is succeed
+- (void)getOrderPaymentStatusSucessWithOrdetStatus:(MFOrderStatusRequestResponse *)ordetStatus{
+    
+    NSLog(@"order is success");
+    
+}
+
+//Delegate method to be called when payment is Fails
+- (void)getOrderPaymentStatusFailedWithOrdetStatus:(MFOrderStatusRequestResponse *)ordetStatus{
+    NSLog(@"order is failed");
+}
+
 
 @end
--- www/FatooraPaymentPluginNewSDK.js
+++ www/FatooraPaymentPluginNewSDK.js
@@ -1,5 +1,13 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'FatooraPaymentPluginNewSDK', 'coolMethod', [arg0]);
+module.exports.presentPaymentModule = function (arg0, success, error) {
+    exec(success, error, 'FatooraPaymentPluginNewSDK', 'presentPaymentModule', [arg0]);
+};
+
+module.exports.testMethod =  function (arg0, success, error) {
+    exec(success, error, 'FatooraPaymentPluginNewSDK', 'testMethod', [arg0]);
+};
+
+module.exports.payNow = function (arg0, success, error) {
+    exec(success, error, 'FatooraPaymentPluginNewSDK', 'payNow', [arg0]);
 };
